6e81fc189c53b9094b7281f23f7bc297cdf4a59b
Thijs Janzen <thijsjanzen@gmail.com>
2020-12-11 15:32:09 +0100

    add ancestry_proportions

--- R/run_ancestry_hmm.R
+++ R/run_ancestry_hmm.R
@@ -8,15 +8,17 @@
 #' with 3 properties: c(ancestor, time, proportion)
 #' @param pulse2 if no cmd_line_options are given, a pulse can be added manually
 #' with 3 properties: c(ancestor, time, proportion)
+#' @param ancestry_proportions ancestry proportions
 #' @param use_genetic_data is the genetic data in reads, or alleles?
 #' @return list with posterior probabilities, age estimate, and likelihood
 #' @export
 run_ancestry_hmm <- function(sample_matrix,
                              genetic_data,
-                             cmd_line_options,
+                             cmd_line_options = numeric(0),
                              viterbi = FALSE,
                              pulse1 = numeric(0),
                              pulse2 = numeric(0),
+                             ancestry_proportions = c(2, 0.5, 0.5),
                              use_genetic_data = TRUE) {
 
   # add input data checks.
@@ -27,6 +29,7 @@ run_ancestry_hmm <- function(sample_matrix,
                                   viterbi,
                                   pulse1,
                                   pulse2,
+                                  ancestry_proportions,
                                   use_genetic_data)
 
   return(results)
