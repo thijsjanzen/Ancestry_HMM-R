c3dc9934da2e0f894b8c201ba80ed3dedac1efd8
Thijs Janzen <thijsjanzen@gmail.com>
2021-01-08 16:11:41 +0100

    add emp data function

--- R/junction_to_anchmm_using_data.R
+++ R/junction_to_anchmm_using_data.R
@@ -1,15 +1,17 @@
 #' converts output from simulations of the junctions package to input for
 #' Ancestry_HMM
 #' @param sim_data  output from junctions::sim_phased_unphased
+#' @param emp_data empirical data.
 #' @return list with posterior probabilities, age estimate, and likelihood
-#' @rawNamespace useDynLib(ancestryhmmR)
-#' @rawNamespace import(Rcpp)
 #' @export
-junctions_to_anchmm <- function(sim_data) {
+junctions_to_anchmm_using_data <- function(sim_data,
+                                           emp_data) {
 
   # we need per line:
   # chrom, pos, A_1, a_1, A_2, a_2, recomdiff, A_indiv1, a_indiv1, A_indiv2 etc
   num_indiv <- length(unique(sim_data$individual))
+
+  indices_emp_data <- seq_along(emp_data$V1)
 
   a1 <- subset(sim_data, sim_data$individual == unique(sim_data$individual)[1])
 
@@ -20,13 +22,15 @@ junctions_to_anchmm <- function(sim_data) {
     testit::assert(length(focal_data$time) == nrow(output_matrix))
 
     for (j in 1:length(focal_data$time)) {
+      emp_data_entry <- emp_data[ sample(indices_emp_data, 1), ]
+
       if (i == unique(sim_data$individual[1])) {
         output_matrix[j, 1] <- 1 # chrom
         output_matrix[j, 2] <- floor(focal_data$location[j] * 1e7)
-        output_matrix[j, 3] <- 100  # A_1
-        output_matrix[j, 4] <- 0 # a_1
-        output_matrix[j, 5] <- 0 # A_2
-        output_matrix[j, 6] <- 100  # a_2
+        output_matrix[j, 3] <- emp_data_entry[[3]]
+        output_matrix[j, 4] <- emp_data_entry[[4]] # a_1
+        output_matrix[j, 5] <- emp_data_entry[[5]] # A_2
+        output_matrix[j, 6] <- emp_data_entry[[6]]  # a_2
 
         recomdiff <- -1
         if (j > 1) {
@@ -39,7 +43,9 @@ junctions_to_anchmm <- function(sim_data) {
       # now we have to count alleles
       genotype <- as.numeric(c(focal_data$anc_chrom_1[j],
                                focal_data$anc_chrom_2[j]))
-      alleles <- c(sum(genotype == 0), sum(genotype == 1))
+
+      alleles <- sample_from_emp(emp_data_entry, genotype)
+
       location_1 <- 8 + 1 * (i * 2)
       location_2 <- location_1 + 1
       output_matrix[j, c(location_1, location_2)] <- alleles
@@ -47,3 +53,17 @@ junctions_to_anchmm <- function(sim_data) {
   }
   return(output_matrix)
 }
+
+#' @keywords internal
+sample_from_emp <- function(emp_data_entry, genotype) {
+  allele_dist <- list(c(emp_data_entry[[3]], emp_data_entry[[4]]),
+                      c(emp_data_entry[[5]], emp_data_entry[[6]]))
+  alleles <- c(0, 0) # options:  0 2  //  1 1 // 2 0
+  for(i in 1:2) {
+    prob_alleles <- allele_dist[[ 1 + genotype[i]]]
+    chosen_allele <- sample(x = 1:2, size = 1, prob = prob_alleles)
+    alleles[chosen_allele] <- alleles[chosen_allele] + 1
+  }
+  return(alleles)
+}
+
